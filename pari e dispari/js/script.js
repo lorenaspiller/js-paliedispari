e2097da4582b954ec480b47563b4d2ec485048be
Lorena Spiller <lorenaspiller@gmail.com>
2021-03-09 11:29:38 +0100

    fix function

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -7,34 +7,15 @@
   Dichiariamo chi ha vinto.
 */
 
-// 1. chiedo all'utente di scegliere tra pari e dispari
-do {
-  var scelta = prompt("Scegli tra pari o dispari");
-} while ( (scelta != "pari") && (scelta != "dispari") );
-console.log(scelta);
+// FUNZIONI
 
-// 2. chiedo all'utente di inserire un numero da 1 a 5
-do {
-  var numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
-} while (isNaN(numeroUtente) || (numeroUtente > 5) || (numeroUtente < 1));
-console.log(numeroUtente);
-
-// 3. Genero un numero random da 1 a 5 con una funzione
-function numeroRandom() {
-  var random = Math.floor(Math.random()* 5) + 1;
+// funzione che genera un numero random
+function numeroRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
   return random;
 }
 
-// 3. Applico la funzione ad una variabile
-var numeroRandom = numeroRandom();
-console.log(numeroRandom);
-
-// 4. Sommo i due numeri
-var sum = parseInt(numeroUtente) + numeroRandom;
-console.log(sum);
-
-// 5. Stabilisco se la somma dei due numeri è pari o dispari usando una funzione
-
+// funzione che determina pari e dispari
 function sommaPariDispari(numero) {
   if (numero % 2 == 0) {
     return "pari";
@@ -43,13 +24,33 @@ function sommaPariDispari(numero) {
   }
 }
 
-// 6. Stabilisco se la somma è pari o dispari applicando la funzione alla somma
-var pariDispari = sommaPariDispari(sum);
-console.log(pariDispari);
 
-// 7. Dichiaro il vincitore
+// 1. chiedo all'utente di scegliere tra pari e dispari
+do {
+  var scelta = prompt("Scegli tra pari o dispari").toLowercase();
+} while ( (scelta != "pari") && (scelta != "dispari") );
+console.log("Scelta dell'utente: " + scelta);
+
+// 2. chiedo all'utente di inserire un numero da 1 a 5
+do {
+  var numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+} while (isNaN(numeroUtente) || (numeroUtente > 5) || (numeroUtente < 1));
+console.log("Numero scelto dall'utente: " + numeroUtente);
+
+// 3. Applico la funzione ad una variabile
+var numeroRandom = numeroRandom(1, 5);
+console.log("Numero random generato per la cpu: " + numeroRandom);
+
+// 4. Sommo i due numeri
+var sum = parseInt(numeroUtente) + numeroRandom;
+console.log("Somma del numero scelto dall'utente e del numero random della cpu " + sum);
+
+// 5. Stabilisco se la somma è pari o dispari applicando la funzione alla somma
+var pariDispari = sommaPariDispari(sum);
+
+// 6. Dichiaro il vincitore
 if (pariDispari === scelta) {
-  console.log("HAI VINTO! Hai scelto " + scelta + " e il numero è " + sum);
+  console.log("HAI VINTO! Hai scelto " + scelta + " e il numero è " + sum + ", quindi è " + pariDispari);
 } else {
-  console.log("NON hai vinto! Hai scelto " + scelta + " e il numero è " + sum);
+  console.log("NON hai vinto! Hai scelto " + scelta + " e il numero è " + sum + ", quindi è " + pariDispari);
 }
